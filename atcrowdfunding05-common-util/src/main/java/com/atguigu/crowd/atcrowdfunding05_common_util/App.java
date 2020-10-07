0aa5d2ee4c5d6f77a19a9f6aba1fb8504ab6a2b9
Stefan-H-Machine <250215145@qq.com>
2020-10-07 21:49:34 +0800

    2020年10月7日 搭建环境

---
+++ atcrowdfunding05-common-util/src/main/java/com/atguigu/crowd/atcrowdfunding05_common_util/App.java
@@ -0,0 +1,13 @@
+package com.atguigu.crowd.atcrowdfunding05_common_util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
